b22866b29c6c310e143d478717f9f824a624ae9d
Joseph Gabino Rodríguez <72351056+alu0101329161@users.noreply.github.com>
2022-10-23 00:33:16 +0100

    gh-pages add

--- deploy.sh
+++ deploy.sh
@@ -12,18 +12,10 @@ cd dist
 # place .nojekyll to bypass Jekyll processing
 echo > .nojekyll
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:alu0101329161/react-recomend-system.git main:gh-pages
-
-cd -
+git push -f git@github.com:alu0101329161/React-Filter-System.git main:gh-pages
