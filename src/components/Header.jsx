aba7cc41811f3fa77d6f87745bd1ab8b178dc1c9
Joseph Gabino Rodríguez <72351056+alu0101329161@users.noreply.github.com>
2022-10-23 12:06:49 +0100

    Añadiendo header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header class="header">
+            <a href="#default" class="logo">CompanyLogo</a>
+            <div class="header-right">
+                <a class="active" href="#home">Home</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Footer from './components/Footer'
+import Header from './components/Header'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header/>
     <App />
     <Footer />
   </React.StrictMode>
